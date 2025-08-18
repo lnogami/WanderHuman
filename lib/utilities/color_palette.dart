93e2539247bd91df3fb8cbd08899f5d4339f9060
inogami <dhetterjan23@gmail.com>
2025-08-18 21:28:54 +0800

    layouting the bottomModalSheet inside home, home > widgets > model-bottom-sheet.dart

--- lib/utilities/color_palette.dart
+++ lib/utilities/color_palette.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 /// A utility class where I define my color palette.
 /// This class provides a centralized place to manage colors used in the app.
 class MyColorPalette {
-  static const Color primaryColor = Color(0xFF6200EE);
+  static const Color darkBlue = Color.fromARGB(255, 1, 42, 146);
 }
