9a42a8989eadc982bec1812b441de97a300efe8f
inogami <dhetterjan23@gmail.com>
2025-08-26 19:58:39 +0800

    implementing firebase to the flutter project, generated firebase_options with flutterfire configure command in the CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:wanderhuman_app/firebase_options.dart';
 import 'package:wanderhuman_app/utilities/color_palette.dart';
 import 'package:wanderhuman_app/view/home/home.dart';
 
 void main() async {
   // this will enure that other components are initialized first before running the whole app
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  // flutter initialization/configuration related
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // Load environment variables from .env file
   await dotenv.load(fileName: ".env");
   runApp(const MainApp());
